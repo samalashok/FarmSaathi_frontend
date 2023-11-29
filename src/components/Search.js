5e4a0e9dc9fb1abc95fd51d0a4b0c30178cdc9df
samalashok <mr.ashok.samal@gmail.com>
2023-11-29 17:10:56 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import Card from './Card'
 import axios from 'axios';
 import { useEffect, useState, useContext } from 'react';
 import '../style/products.css'
-import { Context} from './ContextData';
+import { Context } from './ContextData';
 import { Link } from 'react-router-dom';
 
 export default function Search() {
@@ -22,16 +22,16 @@ export default function Search() {
     return (
         <div className='cards'>
             <h3 className='heading' style={{ color: mode && light }}>Search Result: </h3>
-            {searchText ? <><div className="prods">
+            {searchText ? <div className="prods">
                 {products.filter((prod) => {
                     return prod.name.toLowerCase().includes(searchText.toLowerCase());
                 }).map((prod, i) => {
                     return (<Card className='card-in' id={prod._id} key={i} name={prod.name} desc={prod.desc} price={prod.price} brand={prod.brand} img={prod.img} />)
                 })}
             </div>
-                <p className='disclaim-prod' style={{ color: mode && light }}><i className="fa-solid fa-circle-exclamation"></i>The price shown above may not be the final price. It's only for getting an idea.</p></> : <div style={{ color: mode && light }}>
-                !!!Type something in the search box or go to <Link to="/">home page</Link>!!!
-            </div>}
+                : <div style={{ color: mode && light }}>
+                    !!!Type something in the search box or go to <Link to="/">home page</Link>!!!
+                </div>}
         </div>
     )
 }
